f783787f7c4002f09c9b4d351c1ab91201573077
Jordan Chernof <jordan.chernof@gmail.com>
2018-12-29 10:36:09 -0500

    Added user registration

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 
 @app.route('/')
@@ -43,3 +43,17 @@ def logout():
     logout_user()
     flash('Successfully logged out')
     return redirect(url_for('index'))
+
+@app.route('/register',methods = ['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Welcome, new user! You can now log in.')
+        return redirect(url_for('login'))
+    return render_template('register.html', title ="Register",form=form)
